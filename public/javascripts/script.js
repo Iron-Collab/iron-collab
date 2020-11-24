37af3724cf44603497f5c57363218b5ed490d64b
FunkeFelix <felixfunke456@gmail.com>
2020-11-24 17:02:19 +0100

    filter implemented views & logic

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,9 @@
-// document.addEventListener(
-//   "DOMContentLoaded",
-//   () => {
-//     console.log("IronGenerator JS imported successfully!");
-//   },
-//   false
-// );
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("IronGenerator JS imported successfully!");
+  },
+  false
+);
 
-// const btnLearnMore = document.querySelector(
-//   "body > div > div:nth-child(1) > button"
-// );
-
-// btnLearnMore.addEventListener("click", function () {
-//   console.log("connected");
-// });
+//filter function
--- routes/project.js
+++ routes/project.js
@@ -1,29 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const Project = require("../models/Project");
+const ensureLogin = require("connect-ensure-login");
 
-router.get("/projects", (req, res, next) => {
+router.get("/projects", ensureLogin.ensureLoggedIn(), (req, res, next) => {
   Project.find().then((allProjects) => {
     console.log(allProjects);
     res.render("project/board", { allProjects });
   });
 });
 
-router.get("/new-project", (req, res, next) => {
+router.get("/new-project", ensureLogin.ensureLoggedIn(), (req, res, next) => {
   res.render("project/new_project");
 });
 
-router.get("/project-details/:id", (req, res, next) => {
-  const projectId = req.params.id;
-  Project.findById(projectId)
-    .populate("owner")
-    .then((projectDetails) => {
-      console.log(projectDetails);
-      res.render("project/project_details", { projectDetails });
-    });
-});
+router.get(
+  "/project-details/:id",
+  ensureLogin.ensureLoggedIn(),
+  (req, res, next) => {
+    const projectId = req.params.id;
+    Project.findById(projectId)
+      .populate("owner")
+      .then((projectDetails) => {
+        console.log(projectDetails);
+        res.render("project/project_details", { projectDetails });
+      });
+  }
+);
 
-router.get("/my-projects", (req, res, next) => {
+router.get("/my-projects", ensureLogin.ensureLoggedIn(), (req, res, next) => {
   const id = req.body.id;
   Project.find({ id })
     .then((found) => {
@@ -34,13 +39,33 @@ router.get("/my-projects", (req, res, next) => {
     });
 });
 
-router.post("/new-project", (req, res, next) => {
-  const { title, describtion, deadline, lookingFor } = req.body;
+router.post("/projects", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  const { searchBar } = req.body;
+  const filtered = [];
+  Project.find()
+    .then((found) => {
+      found.forEach((project, index) => {
+        if (project.lookingFor[searchBar] !== null) {
+          filtered.push(project);
+        }
+      });
+    })
+    .then(() => {
+      res.render("project/board", { allProjects: filtered });
+    });
+});
+
+router.post("/new-project", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  const { title, description, deadline, webDev, uxUi, data } = req.body;
   Project.create({
     title,
-    describtion,
+    description,
     deadline,
-    lookingFor,
+    lookingFor: {
+      webDev,
+      uxUi,
+      data,
+    },
     owner: req.user,
   })
     .then((newProject) => {
