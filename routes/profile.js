3c49e87d584187b91da16deb93a3639f369038f7
“Anna <aniawaszak@yahoo.com>
2020-11-24 11:24:02 +0100

    improve profile design, fix profile edit

--- routes/profile.js
+++ routes/profile.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const Project = require('../models/Project');
 const { uploader, cloudinary } = require('../config/cloudinary');
-
 const ensureLogin = require('connect-ensure-login');
 
-router.get('/', ensureLogin.ensureLoggedIn(), (req, res) => {
-  const user = req.user;
-  res.render('profile/profile', { user });
+router.get('/:id', ensureLogin.ensureLoggedIn(), (req, res) => {
+  User.findById(req.params.id).then((user, project) => res.render('profile/profile', { user }))
 });
 
 router.get('/:id/edit', ensureLogin.ensureLoggedIn(), (req, res, next) => {
   User.findById(req.params.id).populate('user.course')
   .then((user) => {
-    console.log('user', user)
-    
+
     let optionsCourse = '';
     ['Web Development', 'UX/UI Design', 'Data Analytics', 'Cybersecurity'].forEach((course) => {
       let selectedCourse = '';
@@ -28,11 +26,19 @@ router.get('/:id/edit', ensureLogin.ensureLoggedIn(), (req, res, next) => {
       selectedLocation = (user.location === location) ? ' selected' : '';
       optionsLocation += `<option value='${location}' ${selectedLocation}>${location}</option>`
     })
-    console.log('options', optionsCourse);
 
     res.render('profile/profileEdit', { user, optionsCourse, optionsLocation });
     })
     .catch(err => next(err))
 });
+
+router.post('/:id', uploader.single('photo'), async (req, res, next) => {
+  const { email, name, lastName, course, location, website, github, profilePicture } = req.body;
+  console.log('THIS IS IT', req.file)
+  const deletePhoto = await cloudinary.uploader.destroy(req.user.profilePicture.publicId);
+  const updateProfile = await User.findByIdAndUpdate(req.params.id, { email, name, lastName, course, location, website, github, profilePicture: { imgPath: req.file.path, publicId: req.file.filename} })
+  Promise.all([deletePhoto, updateProfile])
+  .then(() => res.redirect('/'))
+})
 
 module.exports = router;
